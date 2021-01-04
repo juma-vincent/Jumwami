325fad22b1e3e255227bd8e09b2bcd6e2f625d0c
juma-vincent <vinsoncode@gmail.com>
2021-01-04 14:56:24 +0300

    modified index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ app.get('/', (req,res)=>{
 
 
 
-// if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     //Express will serve up production assets like our main.js or main.css
-    // app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
 
     //Express will serve up the index.html file if it doesn't recognize the route
-//     const path = require('path');
-//     app.get('*', (req, res)=>{
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+    const path = require('path');
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = 5000;
 app.listen(process.env.PORT || PORT);   
